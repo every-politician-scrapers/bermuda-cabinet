64a73cafffbae11a0b11a3c3bcfb943c44ed586c
Tony Bowden <tony@tmtm.com>
2021-08-07 09:06:16 +0300

    add scraper for official site

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -7,12 +7,28 @@ require 'pry'
 class MemberList
   # details for an individual member
   class Member < Scraped::HTML
+    PREFIXES = %w[Dr Lt Col the Hon].freeze
+
     field :name do
-      noko.css('.name').text.tidy
+      unprefixed_name.sub(/,.*/, '')
     end
 
     field :position do
-      noko.css('.position').text.tidy
+      tds[2].text.split('&').map(&:tidy)
+    end
+
+    private
+
+    def tds
+      noko.css('td')
+    end
+
+    def full_name
+      tds[1].text.tidy
+    end
+
+    def unprefixed_name
+      PREFIXES.reduce(full_name) { |current, prefix| current.sub(/#{prefix}.? /i, '') }
     end
   end
 
@@ -28,7 +44,7 @@ class MemberList
     private
 
     def member_container
-      noko.css('.member')
+      noko.css('.article-desc table').xpath('.//tr[td]')
     end
   end
 end
