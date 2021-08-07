e838ddcb0d8632a7ced036cf734df41bb058409f
Tony Bowden <tony@tmtm.com>
2021-08-07 13:19:01 +0300

    Update to every-politician-scraper 0.1.0

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -5,15 +5,14 @@ require 'every_politician_scraper/scraper_data'
 require 'pry'
 
 class MemberList
-  # details for an individual member
-  class Member < Scraped::HTML
+  class Member
     PREFIXES = %w[Dr Lt Col the Hon].freeze
 
-    field :name do
+    def name
       unprefixed_name.sub(/,.*/, '')
     end
 
-    field :position do
+    def position
       tds[2].text.split(/(?:and (?=Minister)|&)/).map(&:tidy)
     end
 
@@ -23,26 +22,16 @@ class MemberList
       noko.css('td')
     end
 
-    def full_name
+    def raw_name
       tds[1].text.tidy
     end
 
     def unprefixed_name
-      PREFIXES.reduce(full_name) { |current, prefix| current.sub(/#{prefix}.? /i, '') }
+      PREFIXES.reduce(raw_name) { |current, prefix| current.sub(/#{prefix}.? /i, '') }
     end
   end
 
-  # The page listing all the members
-  class Members < Scraped::HTML
-    field :members do
-      member_container.flat_map do |member|
-        data = fragment(member => Member).to_h
-        [data.delete(:position)].flatten.map { |posn| data.merge(position: posn) }
-      end
-    end
-
-    private
-
+  class Members
     def member_container
       noko.css('.article-desc table').xpath('.//tr[td]')
     end
