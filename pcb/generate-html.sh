05732a220fde3f84d3a57d315e959ee04ca21d1d
Tony Bowden <tony@tmtm.com>
2021-11-10 16:39:49 +0200

    Include enwiki links

--- pcb/generate-html.sh
+++ pcb/generate-html.sh
@@ -5,7 +5,7 @@ HOLDERS=$(mktemp)
 RAWBIOS=$(mktemp)
 BIO_CSV=$(mktemp)
 
-PERSON_PROPS="en,P31,P18,P21,P27,P1559,P1477,P2561,P735,P734,P1950,P5056,P2652,P569,P19,P570,P22,P25,P26,P40,P3373,P39,P69,P511,P102,P3602"
+PERSON_PROPS="en,P31,P18,P21,P27,P1559,P1477,P2561,P735,P734,P1950,P5056,P2652,P569,P19,P570,P22,P25,P26,P40,P3373,P39,P69,P511,P102,P3602,sitelinks"
 
 # Current holders of each wanted position
 qsv select position wikidata/wanted-positions.csv |
@@ -20,15 +20,15 @@ qsv select person $HOLDERS |
   qsv behead |
   wd data --props $PERSON_PROPS --simplify --time-converter simple-day --keep qualifiers,nontruthy,ranks,nondeprecated > $RAWBIOS
 
-# TODO: prioritise 'preferred'
-echo "id,name,gender,dob,dod,image" > $BIO_CSV
+echo "id,name,gender,dob,dod,image,enwiki" > $BIO_CSV
 jq -r '[
     .id,
     .labels.en,
     (.claims.P21 | sort_by(.rank) | reverse | first.value),
     (.claims.P569 | sort_by(.rank) | reverse | first.value),
     (.claims.P570 | sort_by(.rank) | reverse | first.value),
-    (.claims.P18 | sort_by(.rank) | reverse | first.value)
+    (.claims.P18 | sort_by(.rank) | reverse | first.value),
+    (try (.sitelinks.enwiki) catch null)
   ] | @csv' $RAWBIOS |
   sed -e 's/Q6581097/male/' -e 's/Q6581072/female/' >> $BIO_CSV
 
@@ -39,8 +39,8 @@ jq -r '[
 qsv join position wikidata/wanted-positions.csv position $HOLDERS |
   qsv select position,title,person,start > $TMPFILE
 qsv join person $TMPFILE id $BIO_CSV |
-  qsv select title,name,person,start,gender,dob,dod,image |
-  qsv rename position,person,personID,start,gender,DOB,DOD,image > html/current.csv
+  qsv select title,name,person,start,gender,dob,dod,image,enwiki |
+  qsv rename position,person,personID,start,gender,DOB,DOD,image,enwiki > html/current.csv
 
 # Generate HTML
 erb country="$(jq -r .jurisdiction.name meta.json)" csvfile=html/current.csv -r csv -T- pcb/index.erb > html/index.html
