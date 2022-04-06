30368d14efa28336718d52ac7dbddabe4da4115c
Tony Bowden <tony@tmtm.com>
2022-04-06 18:59:25 +0300

    silence cd -

--- sources/assembly-2012/compare.sh
+++ sources/assembly-2012/compare.sh
@@ -7,4 +7,4 @@ cd $(dirname $0)
 wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | qsv search -s startDate . | qsv sort -s itemLabel | ifne tee wikidata.csv
 bundle exec ruby diff.rb | qsv sort -s itemlabel | qsv select 1,item,itemlabel,party,partylabel,gender | tee diff.csv
 
-cd -
+cd ~-
